3b1cb61021ce0866cc9cad40af09853cc27062ed
Saquib <saquibali158@gmail.com>
2025-04-29 11:42:02 +0530

    Name animation check

--- javascript.js
+++ javascript.js
@@ -39,3 +39,92 @@ const burgerBtn = document.getElementById("burgerBtn");
   closeBtn.addEventListener("click", () => {
     mobileMenu.style.left = "-100%";
   });
+
+
+  const text = "SAQUIB";
+const container = document.getElementById('text-container');
+
+// Create a span for each character
+text.split('').forEach(char => {
+  const span = document.createElement('span');
+  span.textContent = char;
+  container.appendChild(span);
+});
+
+// Initialize all spans to be hidden and below their final position
+function initializeSpans() {
+  const spans = Array.from(container.getElementsByTagName('span'));
+  spans.forEach(span => {
+    span.style.transform = "translateY(200px)";
+    span.style.transition = "transform 0.9s cubic-bezier(0.175, 0.885, 0.32, 1.1)";
+  });
+}
+
+initializeSpans();
+
+let index = 0;
+
+function revealLetterByLetter() {
+  const spans = container.getElementsByTagName('span');
+  if (index < spans.length) {
+    const span = spans[index];
+    span.style.transform = "translateY(0)";  // Move up to the final position
+
+    index++
+    setTimeout(revealLetterByLetter, 40 * index);
+  }
+}
+
+// Trigger the reveal animation
+setTimeout(revealLetterByLetter, 40 * index);  // Delay increases with index
+
+
+gsap.registerPlugin(ScrollTrigger) 
+
+gsap.from('.about_images', {
+  scrollTrigger: {
+    trigger: '.about_images',   // when ".box_2" comes into view
+    opacity:1,
+    scrub:true,
+    end:"center center",
+  },
+  opacity: 0,     // animate to opacity 1
+  duration: 1,     // optional: animation duration
+ 
+});
+
+
+
+const quotes = document.querySelectorAll("#animation");
+
+function setupSplits() {
+  quotes.forEach((quote) => {
+    // Reset if needed
+    if (quote.anim) {
+      quote.anim.progress(1).kill();
+      quote.split.revert();
+    }
+
+    quote.split = SplitText.create(quote, {
+      type: "words,chars",
+      linesClass: "split-line"
+    });
+
+    // Set up the anim
+    quote.anim = gsap.from(quote.split.chars, {
+      scrollTrigger: {
+        trigger: quote,
+        toggleActions: "restart pause resume reverse",
+        start: "center center",
+        markers: { startColor: "#dfdcff", endColor: "transparent" }
+      },
+      duration: 0.6,
+      ease: "circ.out",
+      y: 80,
+      stagger: 0.02
+    });
+  });
+}
+
+ScrollTrigger.addEventListener("refresh", setupSplits);
+setupSplits();
